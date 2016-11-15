35a56614e4a959232015d9ef36dbc1845b18722f
philgoeschl <philipp.goeschl@edu-fh.joanneum.at>
2016-11-15 17:16:32 +0100

    added calcPS

--- app/src/main/java/at/fh/swengb/kwtopscalculator/MainActivity.java
+++ app/src/main/java/at/fh/swengb/kwtopscalculator/MainActivity.java
@@ -2,13 +2,23 @@ package at.fh.swengb.kwtopscalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextInput;
+    private TextView resultView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextInput = (EditText)findViewById(R.id.editTextInput);
+        resultView = (TextView)findViewById(R.id.textViewOutput);
     }
 
     public void calcPS(View view){
@@ -19,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
         double result=0;
 
         try {
-            num1=Double.parseDouble(numString);
+            num1=Double.parseDouble(num1String);
             result=num1*num2;
-            resultView.setText(result);
+            resultView.setText(result+" PS");
         } catch (NumberFormatException e) {
             resultView.setText("WRONG INPUT!");
             e.printStackTrace();
